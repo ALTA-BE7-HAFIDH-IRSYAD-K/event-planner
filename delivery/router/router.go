023661a1d3cdd9d385ac4dbdc85334473c21ef05
abbiyudha <abbiyudha14@gmail.com>
2022-04-13 14:49:04 +0700

    feature event

--- delivery/router/router.go
+++ delivery/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"event-planner/delivery/handler/auth"
+	"event-planner/delivery/handler/event"
 	"event-planner/delivery/handler/user"
 	"event-planner/delivery/middleware"
 	"github.com/labstack/echo/v4"
@@ -11,12 +12,19 @@ func RegisterAuthPath(e *echo.Echo, ah *auth.AuthHandler) {
 	e.POST("/auth", ah.LoginHandler())
 }
 
-func RegisterPath(e *echo.Echo, uh *user.UserHandler) {
+func RegisterPath(e *echo.Echo, uh *user.UserHandler, eh *event.EventHandler) {
 
 	e.GET("/users", uh.GetAllHandler(), middleware.JWTMiddleware())
 	e.GET("/users/profile", uh.GetUserById(), middleware.JWTMiddleware())
 	e.POST("/users", uh.CreateUser())
 	e.DELETE("/users", uh.DeleteUser(), middleware.JWTMiddleware())
 	e.PUT("/users", uh.UpdateUser(), middleware.JWTMiddleware())
+
+	e.GET("/events", eh.GetAllHandler())
+	e.GET("/events/:id", eh.GetEventById())
+	e.GET("/events/profile", eh.GetEventByIdUser(), middleware.JWTMiddleware())
+	e.POST("/events", eh.CreateEvent(), middleware.JWTMiddleware())
+	e.DELETE("/events/:id", eh.DeleteEvent(), middleware.JWTMiddleware())
+	e.PUT("/events/:id", eh.UpdateEvent(), middleware.JWTMiddleware())
 
 }
